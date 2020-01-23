04bd860356cb7d7c242b6a1a3594dd089cc86c7f
DoberBober <pochta.q@bk.ru>
2020-01-23 15:37:49 +0300

    Admin skeleton.

--- metrmetr2/flats/admin.py
+++ metrmetr2/flats/admin.py
@@ -1,3 +1,73 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+# Районы.
+class DistrictAdmin(admin.ModelAdmin):
+	search_fields = ['name']
+
+	fieldsets = (
+		(None, {
+			'fields': (
+				('name', 'slug')
+			),
+		}),
+	)
+
+	list_display = ['name', 'slug']
+
+admin.site.register(District, DistrictAdmin)
+
+# Строительные компании.
+class CompanyAdmin(admin.ModelAdmin):
+	search_fields = ['name']
+
+	fieldsets = (
+		(None, {
+			'fields': (
+				('name', 'slug')
+			),
+		}),
+	)
+
+	list_display = ['name', 'slug']
+
+admin.site.register(Company, CompanyAdmin)
+
+# Этапы строительства.
+class StageAdmin(admin.ModelAdmin):
+	search_fields = ['name']
+
+	fieldsets = (
+		(None, {
+			'fields': (
+				('name', 'slug')
+			),
+		}),
+	)
+
+	list_display = ['name', 'slug']
+
+admin.site.register(Stage, StageAdmin)
+
+# Дома.
+class HouseAdmin(admin.ModelAdmin):
+	search_fields = ['name']
+
+	fieldsets = (
+		(None, {
+			'fields': (
+				('name', 'slug', 'company')
+			),
+		}),
+		(None, {
+			'fields': (
+				('district', 'completion', 'stage')
+			),
+		}),
+	)
+
+	list_display = ['name', 'company', 'district', 'completion', 'stage']
+
+admin.site.register(House, HouseAdmin)
