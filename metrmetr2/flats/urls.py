d25b2ec74593cbc6d02d52e5923971dba1eaf830
DoberBober <pochta.q@bk.ru>
2020-01-25 13:50:52 +0300

    API for company and house.

--- metrmetr2/flats/urls.py
+++ metrmetr2/flats/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-	path('api/v0/all/', AllApartmentsView.as_view(), name='all_aprtments_url'),
+	path('api/v0/all', AllApartmentsView.as_view(), name='all_apartments_url'),
+	path('api/v0/<company_slug>', CompanyView.as_view(), name='company_apartments_url'),
+	path('api/v0/<company_slug>/<house_slug>', HouseView.as_view(), name='house_apartments_url'),
 ]
--- metrmetr2/flats/views.py
+++ metrmetr2/flats/views.py
@@ -6,9 +6,27 @@ from rest_framework.response import Response
 from .models import *
 from .serializers import *
 
+# Все дома и квартиры.
 class AllApartmentsView(APIView):
 	def get(self, request):
 		houses = House.objects.all()
+		houses_serialized = HousesSerializers(houses, many=True)
+
+		return Response(houses_serialized.data)
+
+
+# Одна компания.
+class CompanyView(APIView):
+	def get(self, request, company_slug):
+		houses = House.objects.filter(company_id=company_slug)
+		houses_serialized = HousesSerializers(houses, many=True)
+
+		return Response(houses_serialized.data)
+
+# Один дом.
+class HouseView(APIView):
+	def get(self, request, company_slug, house_slug):
+		houses = House.objects.filter(id=house_slug, company_id=company_slug)
 		houses_serialized = HousesSerializers(houses, many=True)
 
 		return Response(houses_serialized.data)
