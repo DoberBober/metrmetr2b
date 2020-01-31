e4ef5c0c9336741ad8ed5cc13c921a35ea25b59c
DoberBober <pochta.q@bk.ru>
2020-01-31 15:11:06 +0300

    Moved admin panel.

--- metrmetr2/metrmetr2/urls.py
+++ metrmetr2/metrmetr2/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('panel/', admin.site.urls),
 	path(r'nested_admin/', include('nested_admin.urls')),
     path(r'api-auth/', include('rest_framework.urls')),
 	path('', include('flats.urls')),
